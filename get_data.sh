c5551371ecfd6e03ce22e6c556d42bee0773144b
Murray Cadzow <murray.cadzow@otago.ac.nz>
2017-03-07 13:40:02 +1300

    comments in the script for each command

--- get_data.sh
+++ get_data.sh
@@ -1,3 +1,11 @@
+# Download SWC inflammation data
 wget http://swcarpentry.github.io/r-novice-inflammation/files/r-novice-inflammation-data.zip
+
+# Extract the zip archive
 unzip r-novice-inflammation-data.zip
+
+# Remove original zip file
 rm r-novice-inflammation-data.zip
+
+# Create a directory for the results to go
+mkdir results

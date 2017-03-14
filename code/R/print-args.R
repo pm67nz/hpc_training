6335bd083800b42ed1cb54dee497ac973423f756
murray.cadzow <murray.cadzow@otago.ac.nz>
2017-03-15 11:08:31 +1300

    updated formatting and included dates

--- code/R/print-args.R
+++ code/R/print-args.R
@@ -1,7 +1,7 @@
 # print-args.R
 #
 # from Software Carpentry R lesson http://swcarpentry.github.io/r-novice-inflammation/05-cmdline/
-#
+# Author: Murray Cadzow  - University of Otago
 # Date 7 March 2017
 #
 # run: Rscript print-args.R
